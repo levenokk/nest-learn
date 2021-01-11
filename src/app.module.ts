899d03815157afda58a4f40cc013ffa99a871454
levenokk <levenokk@gmail.com>
2021-01-11 23:09:25 +0200

    add production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductionService } from './production/production.service';
+import { ProductionModule } from './production/production.module';
 
 @Module({
-  imports: [],
+  imports: [ProductionModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProductionService],
 })
 export class AppModule {}
